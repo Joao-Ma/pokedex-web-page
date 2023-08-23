3d8222b8dd3831597eb14c1cfcc722ffca322fc3
Joao Marcelo <joao.marceloms@hotmail.com>
2023-08-23 19:47:38 -0300

    adicionado cores e tipos aos pokemons, modelo de pokemon e mecanismo de paginacao

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,34 @@
-function convertPokemonToHTML(pokemon, pokedexNumber, number, type){
-    return `
-    <li class="pokemon">
-        <span class="number">#00${pokemon.order}</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-            <ul class="types">
-                ${convertTypeToLiElement(pokemon.types).join(``)}
-            </ul>
-            <img src="${pokemon.sprites.other.home.front_default}" alt="${pokemon.name}">
-        </div>
-    </li>
-    `
-}
-
-
-
-function convertTypeToLiElement(pokemonTypeSlot){
-    return pokemonTypeSlot.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+let limit = 10
+let offset = 0
 
-pokeApi.getPokemons().then((pokemons = []) =>{
-    /*para cada pokemon na lista pokemons usada como parametro acima: o metodo map ira converte-los em 
-    html utilizando o metodo que 'convertPokemonToHTML' e após isso o metodo join irá transformar em 
-    uma única string, depois essa string será adicionada ao HTML com o metodo innerHTML.*/
-    const newHtml = pokemonList.innerHTML += pokemons.map(convertPokemonToHTML).join('')
-    pokemonList.innerHTML = newHtml
+function loadPokemonItens(offset, limit){
+
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) =>{
+        /*para cada pokemon na lista pokemons usada como parametro acima: o metodo map ira converte-los em 
+        html utilizando o metodo que 'convertPokemonToHTML' e após isso o metodo join irá transformar em 
+        uma única string, depois essa string será adicionada ao HTML com o metodo innerHTML.*/
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#0${pokemon.order}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ul class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join(``)}
+                </ul>
+                <img src="${pokemon.sprite}" alt="${pokemon.name}">
+            </div>
+        </li>
+        `).join('')
+
+        pokemonList.innerHTML = newHtml
+    })
+}
+
+loadPokemonItens(offset,limit)
+
+loadMoreButton.addEventListener('click', () => {
+    limit = limit +10
+    loadPokemonItens(offset, limit)
 })
--- assets/js/pokeapi.js
+++ assets/js/pokeapi.js
@@ -1,15 +1,38 @@
 const pokeApi = {}
 
 pokeApi.getPokemonDetails = (pokemon) =>{
-    return fetch(pokemon.url).then((response) => response.json())
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => { 
+pokeApi.getPokemons = (offset = 15, limit = 15) => { 
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json()) //captura a resposta recebida pelo get na url acima, e transforma em json
         .then((jsonBody) => jsonBody.results) 
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
         .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
         .catch((error) => console.error(error))
 }
+
+//metodo para converter um objeto recebido pela pokeapi em um pokemon
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.name = pokeDetail.name
+    pokemon.order = pokeDetail.id
+    
+    //atribui a variavel types os tipos mapeados trazidos pela requisicao na api
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    //nesse caso, o [type] representa o elemento 0 da lista, caso houvessem mais elementos [type, type2, type3] também funciona.
+    const [type] = types 
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.sprite = pokeDetail.sprites.other.home.front_default
+
+    return pokemon;
+}
+
